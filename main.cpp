cdf34b2f9ed8bfc5f720a9125c8cfa1b67bee1b5
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2024-10-20 15:46:00 +0300

    add comments

--- main.cpp
+++ main.cpp
@@ -4,18 +4,23 @@
 #include <thread>
 #include <chrono>
 
-struct alignas(64) PaddedAtomic
-{
-    std::atomic<size_t> value{0};
-    size_t capacity{0};
-
-private:
-    char padding[64 - sizeof(std::atomic<size_t>)];
-};
-
 template <typename T>
 class ring_buffer
 {
+    struct alignas(64) PaddedAtomic
+    {
+        std::atomic<size_t> value{0};
+
+        // Has its own capacity to avoid false cache sharing capacity variable
+        // between push() and pop()
+        size_t capacity{0};
+
+    private:
+        // Has its own capacity to avoid false cache sharing head and tail
+        // between push() and pop()
+        char padding[64 - sizeof(std::atomic<size_t>)]; // size of cache of common CPUs
+    };
+
 public:
     ring_buffer(size_t capacity) : storage(capacity + 1)
     {
@@ -25,30 +30,65 @@ public:
 
     bool push(T value)
     {
-        size_t curr_head = head.value.load(std::memory_order_acquire);
+        // HEAD in cache ---------------
 
+        // Setting order memory_order_acquire cause head variable in pop() can be modified.
+        // So we MUST use memory_order_acquire and NOT memory_order_relaxed
+        size_t curr_head = head.value.load(std::memory_order_acquire);
+        // -----------------------------
+
+        // TAIL in cache ---------------
+
+        // Setting order memory_order_relaxed cause tail variable only in push() can be modified.
+        // No need to use here memory_order_acquire
         size_t curr_tail = tail.value.load(std::memory_order_relaxed);
         size_t next_tail = (curr_tail + 1) % tail.capacity;
 
         if (next_tail == curr_head)
             return false;
+        // -----------------------------
 
+        // STORAGE in cache ------------
         storage[curr_tail] = std::move(value);
+        // -----------------------------
+
+        // TAIL in cache ---------------
+
+        // Setting order memory_order_release cause we are modifing tail variable
         tail.value.store(next_tail, std::memory_order_release);
+        // -----------------------------
 
         return true;
     }
 
     bool pop(T &value)
     {
+        // TAIL in cache ---------------
+
+        // Setting order memory_order_acquire cause head variable in pop() can be modified.
+        // So we MUST use memory_order_acquire and NOT memory_order_relaxed
         size_t curr_tail = tail.value.load(std::memory_order_acquire);
+        // -----------------------------
+
+        // HEAD in cache ---------------
+
+        // Setting order memory_order_relaxed cause head variable only in push() can be modified.
+        // No need to use here memory_order_acquire
         size_t curr_head = head.value.load(std::memory_order_relaxed);
 
         if (curr_head == curr_tail)
             return false;
+        // -----------------------------
 
+        // STORAGE in cache ------------
         value = std::move(storage[curr_head]);
+        // -----------------------------
+
+        // HEAD in cache ---------------
+
+        // Setting order memory_order_release cause we are modifing head variable
         head.value.store((curr_head + 1) % head.capacity, std::memory_order_release);
+        // -----------------------------
 
         return true;
     }
